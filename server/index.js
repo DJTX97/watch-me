150936f8360d030d3392d974ac5380178cbfc138
DJTX97 <the.10.of.one@gmail.com>
2023-11-18 13:51:09 +0200

    feat: vercel server setup

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./data');
+const express = require("express");
+const cors = require("cors");
+const db = require("./data");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.HOST || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json(db);
-})
+app.get("/", (req, res) => {
+  res.json(db);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`)
-})
+  if (process.env.HOST) {
+    console.log(`Example app listening on ${process.env.HOST}`);
+  } else {
+    console.log(`Example app listening on http://localhost:${port}`);
+  }
+});
